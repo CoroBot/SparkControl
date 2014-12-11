59ea8d4d9dfcedec45056bcb39be7a38888fe8d6
bleucanary <cowens@coroware.com>
2014-12-11 15:06:56 -0500

    Added a Joystick Control module that will recieve joystick information for remote control

---
+++ JoyStickControl.py
@@ -0,0 +1,34 @@
+'''
+CoroBot JoyStick Controller Input Module
+Developer: CoroWare
+Date: December 11, 2014
+Author: Cameron Owens <cowens@coroware.com>
+'''
+
+from Pyside import QtCore, QtGui
+import sys
+import os
+
+Joystick_1_Up = #Insert Value Here
+Joystick_1_Right = #Insert Value Here
+Joystick_1_Down = #Insert Value Here
+Joystick_1_Left = #Insert Value Here
+
+Joystick_2_Up = #Insert Value Here
+Joystick_2_Right = #Insert Value Here
+Joystick_2_Down = #Insert Value Here
+Joystick_2_Left = #Insert Value Here
+
+Left_Bumper_1 = #Insert Value Here
+Left_Bumper_2 = #Insert Value Here
+
+Right_Bumper_1 = #Insert Value Here
+Right_Bumper_2 = #Insert Value Here
+
+Dir_Pad_Up = #Insert Value Here
+Dir_Pad_Right = #Inser Value Here
+Dir_Pad_Down = #Insert Value Here
+Dir_Pad_Left = #Insert Value Here
+
+
+class JoyStickControl(QtGui.QWidget)
