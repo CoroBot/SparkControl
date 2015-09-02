e76200a68d0745e67a034ad5c093ba9b248c26c0
bleucanary <cowens@coroware.com>
2015-09-02 13:57:24 -0500

    Had a line reversed that caused issues with a make check.

--- CameraStreamViewer.py
+++ CameraStreamViewer.py
@@ -10,3 +10,12 @@
 This module is for viewing the camera stream from the Raspberry Pi using
 ZMQ and PiCamera. This module will be extended to include OpenCV tools.
 """
+
+'''
+Standard Python Library Imports
+'''
+
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
